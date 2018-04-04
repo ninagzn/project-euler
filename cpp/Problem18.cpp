6b0c3afb50c9555797fbe6108dbc28df9d2c9fb3
Nina Guzun <nina.guzun@gmail.com>
2018-04-04 22:31:30 +0100

    Code cleanup

--- cpp/Problem18.cpp
+++ cpp/Problem18.cpp
@@ -8,34 +8,34 @@ class Problem18
   public:
 	int getSolution()
 	{
-		int **m = constructSparseMatrix();
+		int **tr = readTriangle();
 		for (int i = 13; i >= 0; --i)
 		{
 			for (int j = 0; j <= i; ++j)
 			{
-				m[i][j] = m[i][j] + std::max(m[i + 1][j], m[i + 1][j + 1]);
+				tr[i][j] = tr[i][j] + std::max(tr[i + 1][j], tr[i + 1][j + 1]);
 			}
 		}
 
-		return m[0][0];
+		return tr[0][0];
 	}
 
-	int **constructSparseMatrix()
+	int **readTriangle()
 	{
-		int **arr = new int *[15];
+		int **tr= new int *[15];
 		std::ifstream inf("Problem18.in");
 		for (int i = 0; i < 15; i++)
 		{
-			arr[i] = new int[i + 1];
+			tr[i] = new int[i + 1];
 			for (int k = 0; k <= i; k++)
 			{
 				std::string strInput;
 				inf >> strInput;
-				arr[i][k] = std::stoi(strInput);
+				tr[i][k] = std::stoi(strInput);
 			}
 		}
 
-		return arr;
+		return tr;
 	}
 };
 
